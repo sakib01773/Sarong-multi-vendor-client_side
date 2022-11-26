c3c0002a0af33c8269be851468250e7ef3e553ae
sakib01773 <sakibshohan1@gmail.com>
2022-11-26 22:39:27 +0600

    Client side uploded

---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,212 @@
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signOut,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile,
+    sendEmailVerification
+  } from "firebase/auth";
+
+
+  import { useEffect, useState } from "react";
+import initial from "../Login/Firebase/firebase.init";
+
+  
+
+  initial()
+
+  const useFirebase=()=>{
+    const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [authError,setAuthError]=useState('')
+  const [admin, setAdmin] = useState(false)
+  const [buyer, setBuyer] = useState(false)
+  const [buyers, setBuyers] = useState(false)
+
+//   navbar toggle 
+const [toggle,setToggle]=useState(false)
+const handleClick=()=>{
+    setToggle(false)
+}
+
+const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+
+//   register email and password 
+const registerUser = (email, password, name,client,contact,profession,address,choose, location, status="pending", navigate) => {
+    // sendUser(email)
+    setIsLoading(true)
+    createUserWithEmailAndPassword(auth, email, password, client,contact,profession,address,choose)
+    
+      .then((userCredential) => {
+        verifyEmail();
+        // const destination = location?.state?.from || '/'
+        // navigate(destination)
+        setError("");
+        
+
+        const newUser = { email, displayName: name,client,contact,profession,address,choose };
+        console.log(newUser)
+        // verifyEmail();
+        setUser(newUser)
+        // verifyEmail();
+        // save use to database 
+        sendUser(email, name,client,contact,profession,address,choose, status="pending", 'POST');
+        // send name to firebase after creation 
+        // verifyEmail();
+        updateProfile(auth.currentUser, {
+          displayName: name
+        }).then(() => {
+          // verifyEmail();
+         })
+          .catch((error) => { })
+          setAuthError('')
+          const destination = location?.state?.from || '/'
+          navigate(destination)
+      })
+      .catch((error) => {
+        // setError(error.message);
+        setAuthError(error.message) 
+      })
+      .finally(() => setIsLoading(false));
+
+  }
+
+  //lOGIN WITW EMAIL AND PASSWORD COUSTM 
+  const loginWithOwnEmailAndPass = (email, password, location, navigate) => {
+    setIsLoading(true)
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const destination = location?.state?.from || '/'
+        navigate(destination)
+        // setError('');
+        setAuthError('')
+      })
+      .catch((error) => {
+        // setError(error.message);
+        setAuthError(error.message)
+      })
+      .finally(() => setIsLoading(false));
+
+  }
+
+  // GOOGLE PROVIDER LOGIN
+  const loginWithGoogle = (location, navigate) => {
+
+    setIsLoading(true)
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user)
+        // save to database 
+        sendUser(user.email, user.displayName, 'PUT')
+        // setError("")
+        setAuthError('');
+        const destination = location?.state?.from || '/'
+        navigate(destination)
+      }).catch(error =>  setAuthError(error.message))
+      .finally(() => setIsLoading(false))
+
+  };
+
+
+  // verifyemail 
+   const verifyEmail=()=>{
+    sendEmailVerification(auth.currentUser)
+  .then((result) => {
+   console.log(result)
+  });
+   }
+
+  //LOG OUT USER METHOD
+  const userLogOut = () => {
+    setIsLoading(true)
+    setToggle(false)
+    signOut(auth)
+      .then(() => {
+
+      }).catch((error) => {
+        setError(error.message)
+      })
+      .finally(() => setIsLoading(false));
+  }
+
+
+  // save user to database 
+  const sendUser = (email, displayName,client,contact,profession,address,choose, status="pending",method) => {
+    const user = { email, displayName, status,client,contact,profession,address,choose };
+    fetch('https://evening-chamber-61046.herokuapp.com/users', {
+      method: method,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(user)
+    })
+    .then()
+    // .then(res => res.json())
+    //   .then(data => {
+    //     console.log(data)
+    //   })
+  }
+
+
+  //OBSERVER USER STATE
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+
+      } else {
+        setUser({});
+      }
+      setIsLoading(false);
+    });
+    return () => unsubscribe;
+  }, [auth]);
+
+  // buyer CONDITIONAL DATALOAD
+  useEffect(() => {
+    fetch(`https://evening-chamber-61046.herokuapp.com/users/${user.email}`)
+      .then(res => res.json())
+      .then(data => {
+        setBuyer(data?.buyer)
+      })
+  }, [user.email])
+  // buyer CONDITIONAL DATALOAD
+  useEffect(() => {
+    fetch(`https://evening-chamber-61046.herokuapp.com/user/${user.email}`)
+      .then(res => res.json())
+      .then(data => {
+        setBuyers(data?.buyers)
+      })
+  }, [user.email])
+
+ // admin role the database 
+ useEffect(()=>{
+  fetch(`https://evening-chamber-61046.herokuapp.com/userLogin/${user.email}`)
+  .then(res=>res.json())
+  .then(data=>setAdmin(data?.admin))
+},[user.email])
+
+  return {
+    user,
+    loginWithGoogle,
+    userLogOut,
+    registerUser,
+    isLoading,
+    error,
+    loginWithOwnEmailAndPass,
+    toggle,
+    setToggle,
+    handleClick,
+    admin,
+    buyer,
+    buyers,
+    authError
+   
+  }
+  }
+
+  export default useFirebase;
---
+++ src/Login/Login/Login.js
@@ -0,0 +1,88 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import './Login.css';
+import { FcGoogle } from 'react-icons/fc'
+import useAuth from '../../Hooks/useAuth';
+import { TextField, Toolbar, Typography,Alert } from "@mui/material";
+
+
+
+
+const Login = () => {
+
+    const { loginWithGoogle, loginWithOwnEmailAndPass,authError } = useAuth()
+
+    //Location & navigate
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    //handle google login here
+    const handleGoogleLogin = () => {
+        loginWithGoogle(location, navigate);
+    };
+
+
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        // console.log(data.email)
+        loginWithOwnEmailAndPass(data.email, data.password, location, navigate)
+    }
+    return (
+      <div className='login-background'>
+          <div className='py-5'>
+            {/* <Header></Header> */}
+            <Container>
+                <Row>
+                <div className='row'>
+                    <div className='col-lg-4'>
+                     <div className='mt-5'>
+                     <img data-aos="zoom-in" height="340" width="360" src='https://i.ibb.co/PYRQwwP/1622955529676.png' alt="" />
+                     </div>
+                    </div>
+                    <div className='col-lg-8'>
+                    <Col md={{ span: 8, offset: 2 }}>
+                        <div className="login-form text-center" style={{background:"#7E2239",borderRadius:"20px"}}>
+                            <h2 className=' text-white'>Login to SARONG</h2>
+                            {/* onSubmit={handleSubmit(onSubmit)} */}
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <input
+                                style={{fontWeight:"500"}}
+                                    className='w-75 mb-3'
+                                    {...register("email", { required: true })}
+                                    placeholder='Enter Email' />
+                                <br />
+
+                                <input
+                                style={{fontWeight:"500"}}
+                                    className='w-75 mb-3'
+                                    {...register("password", { required: true })} placeholder='Enter Password' />
+                                <br />
+
+                                <button className='submit-all' type='submit'>Login</button>
+                            </form>
+                            <div className='login-meta'>
+                                <p className='text-white mt-3'>New to Education <Link to={'/register'}><span className='login-links' style={{color:"#46AADC"}}>Create a free Account</span></Link></p>
+                                <span onClick={handleGoogleLogin} style={{ cursor: "pointer" }} 
+                                // onClick={handleGoogleLogin}
+                                 className='fs-4 text-white'>Continue with <FcGoogle  className='fs-2 google' /></span>
+                            </div>
+
+                            {
+          authError && 
+          <Alert severity="error">{authError}</Alert>
+      }
+                        </div>
+                    </Col>
+                    </div>
+                </div>
+                </Row>
+            </Container>
+          
+        </div>
+      </div>
+    );
+};
+
+export default Login;
